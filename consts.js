ba479e7726f7ed82d58564147da3e6e08b70a5e0
JGhignatti <ghignatti.joao@gmail.com>
2025-03-06 19:01:18 -0300

    refactor: delete unused files and old code

--- consts.js
+++
@@ -1,3 +0,0 @@
-export const SIDE = 800;
-export const PADDING = 32;
-export const CLIENT_SIDE = SIDE - 2 * PADDING;
--- screen-manager.js
+++
@@ -1,6 +0,0 @@
-import { SIDE } from "./consts.js";
-
-export const containerEl = document.getElementById("container");
-
-containerEl.style.width = `${SIDE}px`;
-containerEl.style.height = `${SIDE}px`;
